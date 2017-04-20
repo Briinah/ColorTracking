a8a8f68934a8f84e61f1fc8b44129abc65a8113f
Gilbers, C. (Carolien) <caroliengilbers@gmail.com>
2017-04-20 22:13:07 +0200

    track colored circles

---
+++ ColorTracking/CameraCapture.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ColorTracking
+{
+    partial class CameraCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CamImgBox = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.outImgBox = new Emgu.CV.UI.ImageBox();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CamImgBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outImgBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CamImgBox
+            // 
+            this.CamImgBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CamImgBox.Location = new System.Drawing.Point(12, 12);
+            this.CamImgBox.Name = "CamImgBox";
+            this.CamImgBox.Size = new System.Drawing.Size(547, 423);
+            this.CamImgBox.TabIndex = 2;
+            this.CamImgBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(189, 453);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(136, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // outImgBox
+            // 
+            this.outImgBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outImgBox.Location = new System.Drawing.Point(583, 12);
+            this.outImgBox.Name = "outImgBox";
+            this.outImgBox.Size = new System.Drawing.Size(547, 423);
+            this.outImgBox.TabIndex = 4;
+            this.outImgBox.TabStop = false;
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.Location = new System.Drawing.Point(692, 453);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(75, 23);
+            this.btnBlue.TabIndex = 5;
+            this.btnBlue.Text = "Blue filter";
+            this.btnBlue.UseVisualStyleBackColor = true;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.Location = new System.Drawing.Point(823, 452);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(75, 23);
+            this.btnGreen.TabIndex = 6;
+            this.btnGreen.Text = "Green filter";
+            this.btnGreen.UseVisualStyleBackColor = true;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.Location = new System.Drawing.Point(951, 451);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(75, 23);
+            this.btnYellow.TabIndex = 7;
+            this.btnYellow.Text = "Yellow filter";
+            this.btnYellow.UseVisualStyleBackColor = true;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // CameraCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 488);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.outImgBox);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.CamImgBox);
+            this.Name = "CameraCapture";
+            this.Text = "Camera Output";
+            ((System.ComponentModel.ISupportInitialize)(this.CamImgBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outImgBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox CamImgBox;
+        private System.Windows.Forms.Button btnStart;
+        private Emgu.CV.UI.ImageBox outImgBox;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnYellow;
+    }
+}
+
---
+++ ColorTracking/CameraCapture.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.Util;
+
+namespace ColorTracking
+{
+    public partial class CameraCapture : Form
+    {
+        private Capture capture;
+        private bool captureInProgress;
+        enum Color { Blue, Green, Yellow };
+        Color filter = Color.Blue;
+
+        public CameraCapture()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessFrame(object senter, EventArgs arg)
+        {
+            Image<Bgr, byte> ImageFrame = capture.QueryFrame().ToImage<Bgr,Byte>();
+            CvInvoke.MedianBlur(ImageFrame, ImageFrame, 3);
+            Image<Hsv, byte> hsvImage = ImageFrame.Convert<Hsv, Byte>();
+            
+            // red circle
+            //Image<Gray, byte> redImage = ThresholdImage(hsvImage, new Point[] { new Point(0, 10), new Point(163, 179) });
+
+            // blue circle
+            Image<Gray, byte> blueImage = ThresholdImage(hsvImage, new Point[] { new Point(100, 120)});
+
+            // green circle
+            Image<Gray, byte> greenImage = ThresholdImage(hsvImage, new Point[] { new Point(50, 85) });
+
+            // yellow circle
+            Image<Gray, byte> yellowImage = ThresholdImage(hsvImage, new Point[] { new Point(20, 40) });
+
+            switch(filter)
+            {
+                case Color.Blue: outImgBox.Image = blueImage;
+                    break;
+                case Color.Green: outImgBox.Image = greenImage;
+                    break;
+                case Color.Yellow: outImgBox.Image = yellowImage;
+                    break;
+                default:
+                    break;
+
+            }
+
+            //CamImgBox.Image = DetectAndDrawCircles(redImage, ImageFrame, new MCvScalar(50, 50, 150));
+            var temp = DetectAndDrawCircles(blueImage, ImageFrame, new MCvScalar(150, 50, 0));
+            var temp2 = DetectAndDrawCircles(yellowImage, temp, new MCvScalar(0, 200, 200));
+            CamImgBox.Image = DetectAndDrawCircles(greenImage, temp2, new MCvScalar(50, 150, 0));
+        }
+
+        private Image<Gray,byte> ThresholdImage(Image<Hsv,byte> original, Point[] ranges)
+        {
+            List<Image<Gray, byte>> images = new List<Image<Gray, byte>>();
+            for(int i = 0; i < ranges.Length; i++)
+            {
+                images.Add(original.InRange(new Hsv(ranges[i].X, 70, 100), new Hsv(ranges[i].Y, 255, 255)));
+            }
+
+            Image<Gray, byte> threshold = new Image<Gray, byte>(original.Size);
+            for (int j = 0; j < images.Count; j++)
+            {
+                CvInvoke.AddWeighted(images[j], 1, threshold, 1, 0, threshold);
+            }
+
+            threshold.SmoothGaussian(9);
+
+            Image<Gray, byte> temp = threshold.Clone();
+            var element = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Cross, new Size(3, 3), new Point(-1, -1));
+            for (int n = 0; n < 50; n++)
+            {
+                // opening
+                CvInvoke.Erode(threshold, temp, element, new Point(-1, -1), 1, Emgu.CV.CvEnum.BorderType.Reflect, default(MCvScalar));
+                CvInvoke.Dilate(temp, threshold, element, new Point(-1, -1), 1, Emgu.CV.CvEnum.BorderType.Reflect, default(MCvScalar));
+            }
+            return threshold;
+        }
+
+        private Image<Bgr,byte> DetectAndDrawCircles(Image<Gray, byte> binaryImage, Image<Bgr,byte> imageFrame, MCvScalar color)
+        {
+            // detect circles
+            CircleF[] circles = CvInvoke.HoughCircles(binaryImage, Emgu.CV.CvEnum.HoughType.Gradient, 2, binaryImage.Rows / 8, 100, 50, 0, 100);
+            Image<Bgr, byte> temp = imageFrame.Clone();
+
+            // draw circles
+            if (circles.Length > 0)
+            {
+                Console.WriteLine(circles.Length);
+                for (int current = 0; current < circles.Length; ++current)
+                {
+                    Point center = Point.Round(circles[current].Center);
+                    int radius = (int)Math.Round(circles[current].Radius);
+
+                    CvInvoke.Circle(temp, center, radius, color, 3);
+                }
+            }
+
+            return temp;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(capture == null)
+            {
+                try
+                {
+                    capture = new Capture();
+                }
+                catch(NullReferenceException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                if(captureInProgress)
+                {
+                    btnStart.Text = "Start";
+                    Application.Idle -= ProcessFrame;
+                }
+                else
+                {
+                    btnStart.Text = "Stop";
+                    Application.Idle += ProcessFrame;
+                }
+
+                captureInProgress = !captureInProgress;
+            }
+        }
+
+        private void ReleaseData()
+        {
+            if (capture != null)
+                capture.Dispose();
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            filter = Color.Blue;
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            filter = Color.Green;
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            filter = Color.Yellow;
+        }
+    }
+}
